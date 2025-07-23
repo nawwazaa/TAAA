b60d8a531094646bfea0d05eb44f485420c88ed7
nawwazaa <89263124+nawwazaa@users.noreply.github.com>
2025-07-23 21:18:42 +0300

    Fix missing VoiceAssistant component imports

--- src/features/VoiceAssistant/index.ts
+++ src/features/VoiceAssistant/index.ts
@@ -1,8 +1,5 @@
 export { default as VoiceAssistantSystem } from './VoiceAssistantSystem';
 export { default as VoiceInterface } from './components/VoiceInterface';
-export { default as NotificationReader } from './components/NotificationReader';
-export { default as LocationServices } from './components/LocationServices';
-export { default as VoiceSettings } from './components/VoiceSettings';
 export * from './types';
 export * from './hooks';
 export * from './utils';
